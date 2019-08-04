2992bdbccb3b731e80aeaa624042f927b7d81d76
Priyesh <91priyesh@gmail.com>
2019-08-04 11:44:00 -0400

    going back to class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,29 +6,14 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component'
 
 
-import { WireFrame } from './components/wireframeComponent/wireframe.component';
-import { NavBar } from './components/wireframeComponent/navbar/navbar.component';
-import { PostComp } from './components/wireframeComponent/postcomp/postcomp.component';
-import { PostSearch } from './components/wireframeComponent/postcomp/postSearch/postsearch.component';
-import { PostCompOne } from './components/wireframeComponent/postcomp/postcompone/postcompone.component';
-import { PostCompTwo } from './components/wireframeComponent/postcomp/postcomptwo/postcomptwo.component';
-import { PostCompThree } from './components/wireframeComponent/postcomp/postcompthree/postcompthree.component';
-import { Sidebar } from './components/wireframeComponent/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    WireFrame,
-    NavBar,
-    PostComp,
-    PostSearch,
-    PostCompOne,
-    PostCompTwo,
-    PostCompThree,
-    Sidebar
+    ],
     
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule
